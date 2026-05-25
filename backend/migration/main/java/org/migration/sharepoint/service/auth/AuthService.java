762af99a7acac982fd67c3b58ad801c80108d30c
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2026-05-25 15:02:27 -0300

    [fixing]: Corrigindo limpeza de cookies.

--- backend/migration/main/java/org/migration/sharepoint/service/auth/AuthService.java
+++ backend/migration/main/java/org/migration/sharepoint/service/auth/AuthService.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
